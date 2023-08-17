6b16a0ca7dfe2c91f43a5f82c501a88604b337b6
Femi Abimbola <femiabimbola6@gmail.com>
2023-08-17 17:40:42 +0100

    test

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const Dashboard = () => {
   return (
+    <div>
     <p> The Dashboard (protected)</p>
+    <UserButton afterSignOutUrl="/" />
+    </div>
   )
 }
 
