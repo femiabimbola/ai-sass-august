0064bff7e551d8e7a7e10895a75c0f7ef04074ed
Femi Abimbola <femiabimbola6@gmail.com>
2023-09-18 17:30:28 +0100

    feat: doing the apilimit

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,90 +1,104 @@
 "use client";
 
-import Link from "next/link"
-import Image from "next/image"
-import { Montserrat } from "next/font/google"
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
 
 import { cn } from "@/lib/utils";
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
-const montserrat = Montserrat({ weight: "600", subsets: ["latin"] })
+const montserrat = Montserrat({ weight: "600", subsets: ["latin"] });
 
 const routes = [
   {
     label: "Dashboard",
     icon: LayoutDashboard,
     href: "/dashboard",
-    color: "text-sky-500"
+    color: "text-sky-500",
   },
   {
     label: "Conversation",
     icon: MessageSquare,
     href: "/conversation",
-    color: "text-violet-500"
+    color: "text-violet-500",
   },
   {
     label: "Image Generation",
     icon: ImageIcon,
     href: "/image",
-    color: "text-pink-700"
+    color: "text-pink-700",
   },
   {
     label: "Video Generation",
     icon: VideoIcon,
     href: "/video",
-    color: "text-orange-700"
+    color: "text-orange-700",
   },
   {
     label: "Music Generation",
     icon: Music,
     href: "/music",
-    color: "text-emerald-700"
+    color: "text-emerald-700",
   },
   {
     label: "Code Generation",
     icon: Code,
     href: "/code",
-    color: "text-green-700"
+    color: "text-green-700",
   },
   {
     label: "Settings",
     icon: Settings,
     href: "/settings",
   },
-]
+];
 
+interface SidebarProps {
+  apiLimitCount: number;
+}
 
-const Sidebar = () => {
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   // To hightlight the active link start //
-  const pathname = usePathname()
+  const pathname = usePathname();
   // To hightlight the active link end//
 
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
-        <Link href={"/dashboard"} className="flex items-center pl-3 mb-14">
+        <Link
+          href={"/dashboard"}
+          className="flex items-center pl-3 mb-14"
+        >
           <div className="relative w-8 h-8 mr-4">
-            <Image src={"/logo.png"} fill alt="logo" />
+            <Image
+              src={"/logo.png"}
+              fill
+              alt="logo"
+            />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}> Tush AI </h1>
         </Link>
         <div className="space-y-1">
           {routes.map((route) => (
-            <Link key={route.href} href={route.href}
-              className={cn("dashboard-menu group", pathname === route.href ? 'text-white bg-white/10' : 'text=zinc-400')}
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "dashboard-menu group",
+                pathname === route.href ? "text-white bg-white/10" : "text=zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
                 {route.label}
               </div>
             </Link>
-          ))
-          }
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Sidebar;
